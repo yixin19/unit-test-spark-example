53de3ffb26d75a7bb4acc7da7bec1eae5a0c73c4
yixin19 <yixin.wang.dev@gmail.com>
2022-10-04 13:51:39 +0200

    add transform

--- jobs/pitags_job.py
+++ jobs/pitags_job.py
@@ -1,14 +1,15 @@
 from pyspark.sql import DataFrame, SparkSession
 
-
 # We have a csv file located in raw directory
 # We want to clean pitags name in this file and load the data into bronze layer into parquet format.
 # Perform the following step using spark to do this operation
 
 # 1. Extract: from csv ./raw/pitags.csv
 # 2. Transform: if there's "." at the end of pitag name, remove last ".".
-#   hint: use expresion `when` and `substring`
+#   hint: use expresion with_column,`when` and `substring`
 # 3. load the data into bronze layer
+from pyspark.sql.functions import when, col, regexp_replace
+
 
 class PitagsJob:
     def __init__(self):
@@ -21,7 +22,11 @@ class PitagsJob:
             "../raw/pitags.csv")
 
     def transform(self, pitags_df: DataFrame) -> DataFrame:
-        pass
+        return pitags_df.withColumn(colName="Tags",
+                                    col=self.remove_point_at_last_position("Tags"))
+
+    def remove_point_at_last_position(self, tag_name: str) -> col:
+        return regexp_replace(col(tag_name), "\.$", "")
 
     def load(self, pitags_df: DataFrame):
         pass
--- tests/pitags_test.py
+++ tests/pitags_test.py
@@ -1,3 +1,5 @@
+from pyspark.sql import DataFrame
+
 from jobs.pitags_job import PitagsJob
 
 
@@ -15,10 +17,39 @@ def test_extract():
 
 def test_transform():
     # GIVEN
+    # given_datetime = [{"datetime": convert_string_to_datetime("2022-04-28 21:54:05.605010")}]
+    # date_time_df = spark.createDataFrame(data=given_datetime, schema=PitagsWriterWorker.PITAGS_FILE_SCHEMA)
     pitags_job = PitagsJob()
+    given_pitags_data = [{"Tags": "I_302TK4404_1:T4404.", "date": "2012-01-01"},
+                         {"Tags": "I_302T.K4404_1:T4403.", "date": "2012-01-02"}]
+    given_df = pitags_job.spark.createDataFrame(data=given_pitags_data)
 
     # WHEN
-    pitags_job.transform(pitags_df=None)
+    expected_pitags_data = [{"Tags": "I_302TK4404_1:T4404", "date": "2012-01-01"},
+                            {"Tags": "I_302T.K4404_1:T4403", "date": "2012-01-02"}]
+    expected_df = pitags_job.spark.createDataFrame(data=expected_pitags_data)
+    result_df = pitags_job.transform(pitags_df=given_df)
 
     # THEN
-    assert True
+    assert convert_df_to_dict(result_df) == convert_df_to_dict(expected_df)
+
+
+def test_remove_point_at_last_position():
+    # GIVEN
+    pitags_job = PitagsJob()
+    given_pitags_data = [{"Tags": "I_302TK4404_1:T4404.", "date": "2012-01-01"},
+                         {"Tags": "I_302T.K4404_1:T4403.", "date": "2012-01-02"}]
+    given_pitags_df = pitags_job.spark.createDataFrame(data=given_pitags_data)
+    expected_pitags_data = [{"Tags": "I_302TK4404_1:T4404", "date": "2012-01-01"},
+                            {"Tags": "I_302T.K4404_1:T4403", "date": "2012-01-02"}]
+    expected_df = pitags_job.spark.createDataFrame(data=expected_pitags_data)
+
+    # WHEN
+    got_df = given_pitags_df.withColumn("Tags", pitags_job.remove_point_at_last_position("Tags"))
+
+    # THEN
+    assert convert_df_to_dict(got_df) == convert_df_to_dict(expected_df)
+
+
+def convert_df_to_dict(df_to_convert: DataFrame) -> list[dict]:
+    return df_to_convert.rdd.map(lambda row: row.asDict()).collect()
